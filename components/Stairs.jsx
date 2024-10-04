73ead8324dd1999b94c5815376fd826430c54a4c
Abhishek Chouhan <abhishekchouhannk@gmail.com>
2024-10-04 16:41:37 -0700

    fixed ease in and ease out animation

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -12,18 +12,40 @@ const stairAnimation = {
   },
 };
 
+// calculate the reverse index for staggered delay
+const revereseIndex = (index) => {
+  const totalSteps = 6;
+  return totalSteps - index - 1;
+};
+
 const Stairs = () => {
-  return <>
-  
-  { /* render & motion divs, each representing a step fo the stairs.
+  return (
+    <>
+      {/* render & motion divs, each representing a step fo the stairs.
   
   Each div will have the same animation defined by the stairsAnimation object.
   The delay for each div is calculate sinamically based on it's reversed index,
   creating a staggered effect with decreasing delay for each subsequent step.
   
   */}
-  
-  </>
-}
 
-export default Stairs
+      {[...Array(6)].map((_, index) => {
+        <motion.div
+          key={index}
+          variants={stairAnimation}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          transition={{
+            duration: 0.4,
+            ease: "easeInOut",
+            delay: revereseIndex(index) * 0.1,
+          }}
+          className="h-full w-full bg-white relative"
+        />;
+      })}
+    </>
+  );
+};
+
+export default Stairs;
