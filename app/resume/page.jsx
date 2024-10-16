8cf83626db0cb0998bb6163165352add7e75a9ad
Abhishek Chouhan <abhishekchouhannk@gmail.com>
2024-10-16 03:28:05 -0700

    started development on resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,7 +1,79 @@
-import React from 'react'
+"use client";
 
-const Resume = () => {
-  return <div>Resume</div>
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaFigma,
+  FaNodeJs,
+} from "react-icons/fa";
+
+import { SiTailwindcss, SiNextdotjs } from "react=icons/si";
+
+{ /* about section */}
+const about = {
+  title: "About me",
+  description:
+    "I am a tenacious and hard working developer who is not afraid to take on challenges. With 2 years of experience, I have been constantly learning and bettering myself at coding with each project. Here is my resume for you to see all the details.",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Abhishek Chouhan"
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "+1 604-779-4947"
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "2+"
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "abhishekchouhannk@gmail.com"
+    },
+    {
+      fieldName: "Residency",
+      fieldValue: "BC, Canada"
+    },
+    {
+      fieldName: "Freelance",
+      fieldValue: "Available"
+    },
+    {
+      fieldName: "Languages Spoken",
+      fieldValue: "English, Hindi, Punjabi, French (really basic ;)"
+    },
+  ]
+};
+
+{ /* experience data */}
+const experience = {
+  icon: "/assets/resume/badge.svg",
+  title: "My experience",
+  description: "Lorem Ipsum Dolor Sit Amet",
+  items: [
+    {
+      company: "Intelligent Project Solutions Ltd.",
+      position: "Fullstack Developer",
+      duration: "Sep 2023 - Dec 2023"
+    },
+    {
+      company: "Data Annotation",
+      position: "Contracted Coder/Data Annotator",
+      duration: "Feb 2024 - Present"
+    },
+    {
+      company: "PlaceSpeak Inc.",
+      position: "Cirriculum Web Developer",
+      duration: "May 2024 - June 2024"
+    }
+  ]
 }
 
-export default Resume
+const Resume = () => {
+  return <div>Resume</div>;
+};
+
+export default Resume;
--- components/Header.jsx
+++ components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
         {/* logo */}
         <Link href="/">
           <h1 className="text-4xl font-semibold">
-            Abhishek <span className="text-accent">.</span>
+            Abhi<span className="text-accent">.</span>
           </h1>
         </Link>
         {/* desktop nav and hire me button */}
