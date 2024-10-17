e78c7b90c5954e7f1a5e40bcec8d3a106f8f267f
Abhishek Chouhan <abhishekchouhannk@gmail.com>
2024-10-16 17:34:12 -0700

    added new skills using devicon library

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -278,12 +278,22 @@ const Resume = () => {
               </ul>
               </ScrollArea>
             </TabsContent>
+
+            { /** about content */}
             <TabsContent value="about" className="w-full">
-              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+              <div className="flex flex-col gap-[30px]">
                 <h3 className="text-4xl font-bold">{about.title}</h3>
-                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
-                  {about.description}
-                </p>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{about.description}</p>
+                <ul>
+                  {about.info.map((item, index) => {
+                    return (
+                      <li key={index}>
+                        <span>{item.fieldName}</span>
+                        <span>{item.fieldValue}</span>
+                      </li>
+                    );
+                  })}
+                </ul>
               </div>
             </TabsContent>
           </div>
