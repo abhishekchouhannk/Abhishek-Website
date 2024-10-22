9563b12202193c48551076b61d64558bf642eee2
Abhishek Chouhan <abhishekchouhannk@gmail.com>
2024-10-22 09:19:14 -0700

    basic work/projects page setup:

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,7 +1,112 @@
-import React from 'react'
+"use client";
+
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+const projects = [
+  {
+    num: "01",
+    category: "frontend",
+    title: "project 1",
+    description: "Lorem Ipsum Dolor Sit Amet",
+    stack: [
+      { name: "Html 5" },
+      { name: "Css 3" },
+      { name: "Javascript" },
+      { name: "React" },
+      { name: "NextJS" },
+    ],
+    image: "/assets/work/thumb1.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "02",
+    category: "frontend",
+    title: "project 2",
+    description: "Lorem Ipsum Dolor Sit Amet",
+    stack: [
+      { name: "Html 5" },
+      { name: "Tailwind CSS" },
+      { name: "Typescript" },
+      { name: "React" },
+      { name: "NextJS" },
+    ],
+    image: "/assets/work/thumb1.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "03",
+    category: "AI model",
+    title: "project 3",
+    description: "Lorem Ipsum Dolor Sit Amet",
+    stack: [
+      { name: "Html 5" },
+      { name: "Css 3" },
+      { name: "Javascript" },
+      { name: "React" },
+      { name: "NextJS" },
+    ],
+    image: "/assets/work/thumb3.png",
+    live: "",
+    github: "",
+  },
+];
 
 const Work = () => {
-  return <div>Work</div>
-}
+  const [project, setProject] = useState(projects[0]);
 
-export default Work
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            {/** outline number */}
+            <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+              {project.num}
+            </div>
+            <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 captialize">
+              {project.category} project
+            </h2>
+            {/** project description */}
+            <p className="text-white/60">{project.description}</p>
+            {/** project stack */}
+            <ul className="flex gap-4">
+              {project.stack.map((item, index) => {
+                return (
+                  <li key={index} className="text-xl text-accent">
+                    {item.name}
+                    {/* don't add the last comma */}
+                    {index !== project.stack.length - 1 && ","}
+                  </li>
+                );
+              })}
+            </ul>
+            {/** border */}
+            <div className="border border-white/20"></div>
+          </div>
+          <div className="w-full xl:w-[50%]">slider</div>
+        </div>
+      </div>
+    </motion.section>
+  );
+};
+
+export default Work;
