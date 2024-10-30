f841ab47cebeede14cbd4341813c4ec3ad5bbfd3
Abhishek Chouhan <abhishekchouhannk@gmail.com>
2024-10-30 12:39:37 -0700

    emailJS contact working

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -4,6 +4,10 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 
+// for emailJS
+import { useRef } from "react";
+import emailjs from "emailjs-com";
+
 import {
   Select,
   SelectContent,
@@ -37,6 +41,30 @@ const info = [
 import { motion } from "framer-motion";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_tt514vj", // Service ID from EmailJS Dashboard
+        "template_p9jh6mb", // template ID from dashboard
+        form.current,
+        "fIpe4SpYOL85RivXr" // EmailJS Public key from Dashboard
+      )
+      .then(
+        (result) => {
+          alert("Message sent succesfully!");
+        },
+        (error) => {
+          alert(
+            "Failed to send message. If problem persists, please try again later."
+          );
+        }
+      );
+  };
+
   return (
     <motion.section
       initial={{ opacity: 0 }}
@@ -50,15 +78,38 @@ const Contact = () => {
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/** form */}
           <div className="xl:w-[54%] order-2 xl:order-none">
-            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+            <form ref={form} onSubmit={sendEmail} className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
               <h3 className="text-4xl text-accent"> Let's work together</h3>
-              <p className="text-white/60">I’m always open to new projects and collaborations. Let’s connect and see what we can create together!</p>
+              <p className="text-white/60">
+                I’m always open to new projects and collaborations. Let’s
+                connect and see what we can create together!
+              </p>
               {/** input */}
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <Input type="firstname" placeholder="Firstname" />
-                <Input type="lastname" placeholder="Lastname" />
-                <Input type="email" placeholder="Email Address" />
-                <Input type="phone" placeholder="Phone Number" />
+                <Input
+                  type="text"
+                  name="firstname"
+                  placeholder="Firstname"
+                  required
+                />
+                <Input
+                  type="text"
+                  name="lastname"
+                  placeholder="Lastname"
+                  required
+                />
+                <Input
+                  type="email"
+                  name="email"
+                  placeholder="Email Address"
+                  required
+                />
+                <Input
+                  type="text"
+                  name="phone"
+                  placeholder="Phone Number"
+                  required
+                />
               </div>
               {/** select */}
               <Select>
@@ -68,10 +119,14 @@ const Contact = () => {
                 <SelectContent>
                   <SelectGroup>
                     <SelectLabel>Select a service</SelectLabel>
-                    <SelectItem value="est">Web Development</SelectItem>
-                    <SelectItem value="cst">UI/UX Design</SelectItem>
-                    <SelectItem value="mst">Software Development</SelectItem>
-                    <SelectItem value="pst">
+                    <SelectItem value="Web Development">
+                      Web Development
+                    </SelectItem>
+                    <SelectItem value="UI/UX Design">UI/UX Design</SelectItem>
+                    <SelectItem value="Software Development">
+                      Software Development
+                    </SelectItem>
+                    <SelectItem value="SEO">
                       Search Engine Optimization
                     </SelectItem>
                   </SelectGroup>
@@ -79,26 +134,32 @@ const Contact = () => {
               </Select>
               {/** textarea */}
               <Textarea
+                name="message"
                 className="h-[200px]"
                 placeholder="Type your message here."
+                required
               />
-              { /** btn */}
-              <Button size="lg" className="max-w-40">Send Message</Button>
+              {/** btn */}
+              <Button type="submit" size="lg" className="max-w-40">
+                Send Message
+              </Button>
             </form>
           </div>
           {/** info */}
           <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
             <ul className="flex flex-col gap-10">
               {info.map((item, index) => {
-                return <li key={index} className="flex items-center gap-6">
-                  <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-xl flex items-center justify-center">
-                    <div className="text-[28px]">{item.icon}</div>
-                  </div>
-                  <div className="flex-1">
-                    <p className="text-white/60">{item.title}</p>
-                    <h3 className="text-xl">{item.description}</h3>
-                  </div>
-                </li>
+                return (
+                  <li key={index} className="flex items-center gap-6">
+                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-xl flex items-center justify-center">
+                      <div className="text-[28px]">{item.icon}</div>
+                    </div>
+                    <div className="flex-1">
+                      <p className="text-white/60">{item.title}</p>
+                      <h3 className="text-xl">{item.description}</h3>
+                    </div>
+                  </li>
+                );
               })}
             </ul>
           </div>
