02eed5c0379ac99a4b06cf93c6e626fe3a9535a5
Abhishek Chouhan <abhishekchouhannk@gmail.com>
2024-11-06 11:22:35 -0800

    update favicon to logo

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,6 +21,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={jetbrainsMono.variable}>
         <Header />
         <StairTransition />
